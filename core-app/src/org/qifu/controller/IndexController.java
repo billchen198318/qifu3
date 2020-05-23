2f3e63798757e19b177e1392340fdbc0044057a2
Bill chen <chen.xin.nien@gmail.com>
2020-05-23 11:39:36 +0800

    update

---
+++ core-app/src/org/qifu/controller/IndexController.java
@@ -0,0 +1,20 @@
+package org.qifu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index(ModelMap mm, HttpServletRequest request) {
+		
+		mm.addAttribute("testMessage", "測試beetl ~~ !!");
+		
+		return "index.html";
+	}
+	
+}
