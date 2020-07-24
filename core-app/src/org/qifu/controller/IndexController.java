9bc96f3ac39663c5f1fcca8e3917ca9958def9b4
billchen198318 <chen.xin.nien@gmail.com>
2020-07-24 15:42:07 +0800

    update

--- core-app/src/org/qifu/controller/IndexController.java
+++ core-app/src/org/qifu/controller/IndexController.java
@@ -2,12 +2,13 @@ package org.qifu.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.qifu.base.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class IndexController extends BaseController {
 	
 	@RequestMapping("/")
 	public String index(ModelMap mm, HttpServletRequest request) {
