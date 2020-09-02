d9e56217aa15149080a2e50d6b91ddc39d7f7be0
billchen198318 <chen.xin.nien@gmail.com>
2020-09-02 08:08:51 +0800

    add thirdparty lib file.

---
+++ core-app/resources/static/js/m.js
@@ -0,0 +1,12 @@
+
+function logoutEvent() {
+	bootbox.confirm(
+			"Logout! are you sure?", 
+			function(result){ 
+				if (!result) {
+					return;
+				}
+				window.location = qifu_basePath + "logout";
+			}
+	);	
+}
