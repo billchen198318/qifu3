11624ef33647ef700bc17f04b112a0c3b7a13a22
billchen198318 <chen.xin.nien@gmail.com>
2020-09-27 14:32:31 +0800

    update

--- core-base/src/org/qifu/core/mapper/TbSysExprJobMapper.java
+++ core-base/src/org/qifu/core/mapper/TbSysExprJobMapper.java
@@ -21,9 +21,11 @@
  */
 package org.qifu.core.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.qifu.base.mapper.IBaseMapper;
 import org.qifu.core.entity.TbSysExprJob;
 
+@Mapper
 public interface TbSysExprJobMapper extends IBaseMapper<TbSysExprJob, String> {
 	
 }
